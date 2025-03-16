2d5003cc6644cf15b8c9aec22ac80aea7e67948b
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-03-17 01:19:41 +0300

    .

---
+++ scripts/generate.py
@@ -0,0 +1,31 @@
+import os
+
+from joblib import Parallel, delayed
+
+import dgsp.model as model
+
+
+NUM_TRAJECTORIES = 10000
+
+if not os.path.exists("data/traj"):
+    os.makedirs("data/traj")
+
+if not os.path.exists("data/meas"):
+    os.makedirs("data/meas")
+
+
+def generate_one(idx: int):
+    system = model.RobotSystem()
+    system.simulate()
+    traj_filename = os.path.join("data", "traj", f"{idx}.npy")
+    meas_filename = os.path.join("data", "meas", f"{idx}.npy")
+    system.save(traj_filename, meas_filename)
+
+
+def generate_all(parallel=True):
+    if parallel:
+        Parallel(n_jobs=-1, verbose=10)(
+            delayed(generate_one)(i) for i in range(NUM_TRAJECTORIES)
+        )
+    else:
+        [generate_one(i) for i in range(NUM_TRAJECTORIES)]
