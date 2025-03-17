1f55a6a19121ef738516e6a5f25a2a680fd20f52
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-03-17 21:48:49 +0300

    .

--- scripts/generate.py
+++ scripts/generate.py
@@ -6,6 +6,8 @@ import dgsp.model as model
 
 
 NUM_TRAJECTORIES = 10000
+T_MAX = 10.0
+dt = 0.0025
 
 if not os.path.exists("data/traj"):
     os.makedirs("data/traj")
@@ -15,7 +17,7 @@ if not os.path.exists("data/meas"):
 
 
 def generate_one(idx: int):
-    system = model.RobotSystem()
+    system = model.RobotSystem(dt=dt, t_max=T_MAX)
     system.simulate()
     traj_filename = os.path.join("data", "traj", f"{idx}.npy")
     meas_filename = os.path.join("data", "meas", f"{idx}.npy")
