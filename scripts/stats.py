a8885d2e496092538d91133a505586f3c247bdd5
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-04-10 12:07:26 +0300

    .

--- scripts/stats.py
+++ scripts/stats.py
@@ -2,12 +2,13 @@ import os
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from dgsp.functions import dim_state
 from scripts import ESTIMATORS, T_MAX, dt_pred
 
 
-def stats() -> None:
+def example() -> None:
     example_traj_num = 0
 
     traj = np.load(os.path.join("data", "traj", f"{example_traj_num}.npy"))
@@ -59,3 +60,21 @@ def stats() -> None:
         )
         df_x.to_csv(os.path.join("stats", f"example_x_{component}.csv"), index=False)
         df_k.to_csv(os.path.join("stats", f"example_k_{component}.csv"), index=False)
+
+    def plot(i: int) -> None:
+        df = pd.read_csv(os.path.join("stats", f"example_x_{i}.csv"))
+        plt.plot(df["t"], df["x"])
+        plt.plot(df["t"], df["ukf"])
+        plt.plot(df["t"], df["ukfr"])
+        plt.plot(df["t"], df["pf"])
+
+    if not os.path.exists(os.path.join("img", "example")):
+        os.makedirs(os.path.join("img", "example"))
+
+    for i in range(dim_state):
+        plot(i)
+        plt.savefig(os.path.join("img", "example", f"estimate_{i}.png"))
+
+
+def stats() -> None:
+    example()
