3f2705e1e1257d28fc431a6111fc91a61bce76ef
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-04-10 16:16:11 +0300

    .

--- dgsp/estimators/base.py
+++ dgsp/estimators/base.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from dgsp.functions import initial, Q
+from scripts import dt_sim, dt_pred
 
 
 class Estimator:
@@ -8,11 +9,14 @@ class Estimator:
     time: float
     state: list[np.ndarray]
     k: list[np.ndarray]
+    Q: np.ndarray
+    R: np.ndarray
 
     def __init__(
         self,
         dt: float,
     ) -> None:
+        self.Q = Q / dt_sim * dt_pred
         self.dt = dt
         self.state = [initial]
         self.k = [Q]
