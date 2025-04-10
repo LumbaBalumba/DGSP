9984b110ec9a6972a9878f4f46a79cdbdf772a3f
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-04-10 14:00:59 +0300

    errs

--- scripts/stats.py
+++ scripts/stats.py
@@ -1,12 +1,14 @@
 import os
-from typing import Literal
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 from dgsp.functions import dim_state
 from scripts import ESTIMATORS, T_MAX, dt_pred
+
+COLORS = ["red", "blue", "orange", "green", "black"]
 
 
 def example() -> None:
@@ -62,26 +64,64 @@ def example() -> None:
         df_x.to_csv(os.path.join("stats", f"example_x_{component}.csv"), index=False)
         df_k.to_csv(os.path.join("stats", f"example_k_{component}.csv"), index=False)
 
-    def plot(i: int, data: Literal["x"] | Literal["k"] = "x") -> None:
-        df = pd.read_csv(os.path.join("stats", f"example_{data}_{i}.csv"))
-        if data == "x":
-            plt.plot(df["t"], df["x"])
-        plt.plot(df["t"], df["ukf"])
-        plt.plot(df["t"], df["ukfr"])
-        plt.plot(df["t"], df["pf"])
+    def plot_estimate(i: int) -> None:
+        df = pd.read_csv(os.path.join("stats", f"example_x_{i}.csv"))
+
+        plt.figure(figsize=(20, 10))
+        for estimator in ESTIMATORS:
+            plt.plot(df["t"], df[estimator], label=estimator.upper())
+
+        plt.legend()
 
     if not os.path.exists(os.path.join("img", "example")):
         os.makedirs(os.path.join("img", "example"))
 
     for i in range(dim_state):
-        plot(i, "x")
+        plot_estimate(i)
         plt.savefig(os.path.join("img", "example", f"estimate_{i}.png"))
         plt.clf()
 
+    def plot_err(i: int):
+        df_x = pd.read_csv(os.path.join("stats", f"example_x_{i}.csv"))
+        df_k = pd.read_csv(os.path.join("stats", f"example_k_{i}.csv"))
+
+        df_x = df_x[len(df_x) // 1000 * 5 :]
+        df_k = df_k[len(df_k) // 1000 * 5 :]
+
+        plt.figure(figsize=(20, 10))
+        for estimator in ESTIMATORS:
+            t = df_x["t"]
+            err = df_x[estimator] - df_x["x"]
+            sigma = df_k[estimator] ** 0.5
+
+            plt.plot(
+                t,
+                err,
+                label=f"Error {estimator.upper()}",
+                color="black",
+            )
+            plt.plot(
+                t,
+                sigma,
+                label=rf"$\sigma$ {estimator.upper()}",
+                color="green",
+                marker=".",
+            )
+            plt.plot(t, -sigma, color="green", marker=".")
+            plt.plot(
+                t,
+                sigma * 3,
+                label=rf"$3\sigma$ {estimator.upper()}",
+                color="red",
+                marker="_",
+            )
+            plt.plot(t, -sigma * 3, color="red", marker="+")
+            plt.legend()
+            plt.savefig(os.path.join("img", "example", f"err_{i}_{estimator}.png"))
+            plt.clf()
+
     for i in range(dim_state):
-        plot(i, "k")
-        plt.savefig(os.path.join("img", "example", f"err_{i}.png"))
-        plt.clf()
+        plot_err(i)
 
 
 def stats() -> None:
