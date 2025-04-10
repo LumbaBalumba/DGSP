c925faa5b246035d043c48527f9e26ab4491b5a7
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-04-10 12:24:53 +0300

    j and h

--- dgsp/functions.py
+++ dgsp/functions.py
@@ -26,7 +26,12 @@ sp_transition = sp.Matrix(
         u2,
     ]
 )
+sp_transition_j = sp_transition.jacobian(x)
+sp_transition_h = sp_transition_j.jacobian(x)
+
 sp_observation = sp.Matrix([(x1**2 + x2**2) ** 0.5, sp.atan2(x2, x1)])
+sp_observation_j = sp_observation.jacobian(x)
+sp_observation_h = sp_observation_j.jacobian(x)
 
 dim_state = sp_transition.shape[0]
 dim_observation = sp_observation.shape[0]
@@ -38,7 +43,12 @@ def prettify(func):
 
 
 transition = prettify(sp_transition)
+transition_j = prettify(sp_transition_j)
+transition_h = prettify(sp_transition_h)
+
 observation = prettify(sp_observation)
+observation_j = prettify(sp_observation_j)
+observation_h = prettify(sp_observation_h)
 
 
 @numba.njit()
