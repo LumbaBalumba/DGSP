b37f12d1a19c5327efab57f6c995a7ba0140a2a5
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-04-20 15:05:16 +0300

    .

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,10 +3,8 @@ dt_sim = 1e-4
 dt_pred = dt_sim
 dt_obs = dt_pred * 10
 
-NUM_TRAJECTORIES = 10
+NUM_TRAJECTORIES = 1000
 
-ESTIMATORS = [
-    "pf",
-]
+ESTIMATORS = ["ckf", "ukf", "trivial"]
 
 ENABLE_PARALLEL = True
